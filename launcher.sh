3fe13375fc30af2c26578f535090b378d293c7bb
Suraj Mavuleti <surajmavuleti@gmail.com>
2026-09-06 20:13:59 +0530

    Feat(v8): Visual Overhaul using CustomTkinter Modern UI Framework

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# ARCH LINUX NATIVE AUTO-UPDATER
 cd /home/suraj/.gemini/antigravity/scratch/heavy_suite/zero-physics-arch-linux
 git pull origin main --quiet
-python3 zero_physics_gui.py
+/home/suraj/.gemini/antigravity/scratch/v8_env/bin/python3 zero_physics_gui.py
